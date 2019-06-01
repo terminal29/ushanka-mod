674f341ff93c40c349559b0dcb2db85f919dab79
terminal29 <20899505+terminal29@users.noreply.github.com>
2019-06-01 22:40:21 +1000

    Update ModInfo.java

--- src/main/java/com/terminal29/ushanka/ModInfo.java
+++ src/main/java/com/terminal29/ushanka/ModInfo.java
@@ -2,34 +2,62 @@ package com.terminal29.ushanka;
 
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class ModInfo {
     public static final String DISPLAY_NAME = "ushanka";
-    public class Dimensions {
-        public static final String DIMENSION_NAME = "village";
-        public static final String BIOME = "village";
+
+    // Dimensions
+    public static final String DIMENSION_NAME = "village";
+    public static final String DIMENSION_BIOME = "village";
+
+    //Keybinds
+    public static final String KEYBIND_CATEGORY = "controls." + ModInfo.DISPLAY_NAME;
+    public static final String KEYBIND_ISO_SCALE_UP = "iso_scale_up";
+    public static final String KEYBIND_ISO_SCALE_DOWN = "iso_scale_down";
+    public static final String KEYBIND_ISO_CAMERA_LEFT = "iso_camera_left";
+    public static final String KEYBIND_ISO_CAMERA_RIGHT = "iso_camera_right";
+    public static final String KEYBIND_ISO_CAMERA_TOGGLE = "iso_camera_toggle";
+
+    //Items
+    public static final String ITEM_USHANKA = "ushanka";
+
+    // Packets
+    public static final String PACKET_CHANGE_DIMENSION = "change_dimension";
+    public static final String PACKET_ISO_STATE = "iso_state";
+    public static final String PACKET_ISO_DIRECTION = "iso_direction";
+    public static final String PACKET_CLIENT_READY = "client_ready";
+
+    // Islands
+    public static final String ISLAND_STRUCTURE_0 = "island_0";
+
+    private static List<String> islandList = Arrays.asList(
+            ISLAND_STRUCTURE_0
+    );
+
+    private final String theName;
+    ModInfo(String theName){
+        this.theName = theName;
+    }
+
+    @Override
+    public String toString(){
+        return this.theName;
     }
 
     public static Identifier identifierFor(String object){
         return new Identifier(ModInfo.DISPLAY_NAME, object);
     }
 
-    public class Keybinds {
-        public static final String KEYBIND_CATEGORY = "controls." + ModInfo.DISPLAY_NAME;
-        public static final String ISO_SCALE_UP = "iso_scale_up";
-        public static final String ISO_SCALE_DOWN = "iso_scale_down";
-        public static final String ISO_CAMERA_LEFT = "iso_camera_left";
-        public static final String ISO_CAMERA_RIGHT = "iso_camera_right";
-        public static final String ISO_CAMERA_TOGGLE = "iso_camera_toggle";
+    public static int numIslandStructures(){
+        return islandList.size();
     }
 
-    public class Items {
-        public static final String USHANKA = "ushanka";
-    }
-
-    public class Packets {
-        public static final String CHANGE_DIMENSION = "change_dimension";
-        public static final String ISO_STATE = "iso_state";
-        public static final String ISO_DIRECTION = "iso_direction";
-        public static final String CLIENT_READY = "client_ready";
+    public static Identifier getStructureIdentifier(int id){
+        return identifierFor(islandList.get(id));
     }
 }
+
